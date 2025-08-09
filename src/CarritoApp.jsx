11181d7561ee03ff292216c577f740aed30bb70f
javier <javier.morabaiz@gmail.com>
2025-08-09 08:20:55 -0300

    context of products and product page

--- src/CarritoApp.jsx
+++ src/CarritoApp.jsx
@@ -3,17 +3,20 @@ import { NavBarComponent } from './components/NavBarComponent'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { ProductsPage } from './pages/ProductsPage'
 import { CartPage } from './pages/CartPage'
+import { ProductProvider } from './context/ProductProvider'
 
 
 export const CarritoApp = () => {
     return (
-        <>
+        <ProductProvider>
             <NavBarComponent />
-            <Routes>
-                <Route path="/" element={<ProductsPage />}></Route>
-                <Route path="/cart" element={<CartPage />}></Route>
-                <Route path="/*" element={< Navigate to="/" />}></Route>
-            </Routes>
-        </>
+            <div className="container mt-3">
+                <Routes>
+                    <Route path="/" element={<ProductsPage />}></Route>
+                    <Route path="/cart" element={<CartPage />}></Route>
+                    <Route path="/*" element={< Navigate to="/" />}></Route>
+                </Routes>
+            </div>
+        </ProductProvider>
     )
 }
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -2,30 +2,12 @@ import React, { use } from 'react'
 import { useEffect, useState } from 'react'
 import Swal from 'sweetalert2'
 import { CardComponent } from '../components/CardComponent'
+import { ProductContext } from '../context/ProductContext'
+import { useContext } from 'react'
 
 
 export const ProductsPage = () => {
-
-    const [products, setProducts] = useState([])
-
-    const fetchProducts = async() => {
-        try {
-            const response = await fetch('https://fakestoreapi.com/products')
-            const data = await response.json()
-            setProducts(data)
-        } catch (error) {
-            Swal.fire({
-                icon: 'error',
-                title: '¡Error!',
-                text: 'Hubo un error al obtener los productos',
-            })
-            console.error(error)
-        }
-    }
-
-    useEffect(() => {
-        fetchProducts()
-    }, [])
+    const { products } = useContext(ProductContext)
 
     return (
         <>
@@ -33,10 +15,11 @@ export const ProductsPage = () => {
             <hr />
             {products.map(product => (
                 <CardComponent
-                image={product.image}
-                title={product.title}
-                description={product.description}
-                price={product.price}
+                    key={product.id}
+                    image={product.image}
+                    title={product.title}
+                    description={product.description}
+                    price={product.price}
                 />
             ))}
         </>
